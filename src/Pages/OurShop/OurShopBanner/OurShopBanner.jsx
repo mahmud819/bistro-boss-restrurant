bfdc3a1fb7cfe3a2a25eea2a1d07199296bc36d3
mahmud819 <mahmudurr819@gmail.com>
2026-06-04 07:00:03 +0600

    our shop page react tab add

--- src/Pages/OurShop/OurShopBanner/OurShopBanner.jsx
+++ src/Pages/OurShop/OurShopBanner/OurShopBanner.jsx
@@ -1,13 +1,39 @@
-import React from 'react';
-import SharedBackground from '../../../Shared/SharedComponent/SharedBackground/SharedBackground';
-import ourShopBgImg from '../../../assets/shop/banner2.jpg'
+import React, { useState } from "react";
+import SharedBackground from "../../../Shared/SharedComponent/SharedBackground/SharedBackground";
+import ourShopBgImg from "../../../assets/shop/banner2.jpg";
+import { Tab, TabList, TabPanel, Tabs } from "react-tabs";
+import 'react-tabs/style/react-tabs.css';
 
 const OurShopBanner = () => {
-    return (
-        <div>
-            <SharedBackground bgImage={ourShopBgImg} title={'Our Shop'} paragraph={'Would you like to try a dish?'}></SharedBackground>
-        </div>
-    );
+
+    const [tabIndex, setTabIndex] = useState(0);
+
+
+  return (
+    <div>
+      <SharedBackground
+        bgImage={ourShopBgImg}
+        title={"Our Shop"}
+        paragraph={"Would you like to try a dish?"}
+      ></SharedBackground>
+      <div>
+        <Tabs selectedIndex={tabIndex} onSelect={(index) => setTabIndex(index)}>
+          <TabList>
+            <Tab>SALAD</Tab>
+            <Tab>PIZZA</Tab>
+            <Tab>SOUPS</Tab>
+            <Tab>DESSERTS</Tab>
+            <Tab>DRINKS</Tab>
+          </TabList>
+          <TabPanel>Salad</TabPanel>
+          <TabPanel>Pizza</TabPanel>
+          <TabPanel>Soups</TabPanel>
+          <TabPanel>Desserts</TabPanel>
+          <TabPanel>Drinks</TabPanel>
+        </Tabs>
+      </div>
+    </div>
+  );
 };
 
 export default OurShopBanner;
