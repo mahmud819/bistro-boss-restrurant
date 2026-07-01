cb613c8177c112483a2b8bcf4ad53266dde3cc7d
mahmud819 <mahmudurr819@gmail.com>
2026-07-01 23:13:41 +0600

    contact page location section added sucess

--- src/Pages/ContactUs/ContactUs/ContactCard/ContactCard.jsx
+++ src/Pages/ContactUs/ContactUs/ContactCard/ContactCard.jsx
@@ -1,13 +1,31 @@
-import React from 'react';
-import SharedContactCard from '../../../../Shared/SharedComponent/SharedContactCard/SharedContactCard';
-import { FaPhone } from 'react-icons/fa6';
+import React from "react";
+import SharedContactCard from "../../../../Shared/SharedComponent/SharedContactCard/SharedContactCard";
+import { FaClock, FaLocationPin, FaPhone } from "react-icons/fa6";
+import SharedTitle from "../../../../Shared/SharedComponent/SharedTitle/SharedTitle";
 
 const ContactCard = () => {
-    return (
-        <div>
-            <SharedContactCard icon={<FaPhone></FaPhone>} titile={'Phone'} paragraph={'+8801125-125436'}></SharedContactCard>
-        </div>
-    );
+  return (
+    <div className="py-8">
+        <SharedTitle title={'Our Location'} subTitle={'Visit Us'}></SharedTitle>
+      <div className="grid grid-cols-3 gap-2 p-4">
+        <SharedContactCard
+          icon={<FaPhone></FaPhone>}
+          titile={"Phone"}
+          paragraph={"+8801125-125436"}
+        ></SharedContactCard>
+        <SharedContactCard
+          icon={<FaLocationPin></FaLocationPin>}
+          titile={"Address"}
+          paragraph={"Banani,Dhaka,1250"}
+        ></SharedContactCard>
+        <SharedContactCard
+          icon={<FaClock></FaClock>}
+          titile={"Working Hours"}
+          paragraph={"07:00am to 11:00pm"}
+        ></SharedContactCard>
+      </div>
+    </div>
+  );
 };
 
 export default ContactCard;
--- src/Shared/SharedComponent/SharedContactCard/SharedContactCard.jsx
+++ src/Shared/SharedComponent/SharedContactCard/SharedContactCard.jsx
@@ -3,13 +3,15 @@ import { FaPhone } from 'react-icons/fa6';
 
 const SharedContactCard = ({icon,titile,paragraph}) => {
     return (
-        <div className='w-[420px]'>
+        <div className='w-[100%] h-[300px] z-50 shadow-lg backdrop-blur-md rounded-sm'>
             <div className='bg-[#D99904] flex justify-center py-4 text-lg text-white'>
                 {icon}
             </div>
-            <div className='bg-[#f3f3f3] flex flex-col justify-center items-center pt-8 pb-16'>
+            <div className=' flex justify-center items-center'>
+                <div className='w-[90%] h-[230px] bg-[#f3f3f3] flex flex-col justify-center items-center pt-8 pb-16'>
                 <h1 className='font-bold text-xl'>{titile}</h1>
                 <p>{paragraph}</p>
+            </div>
             </div>
         </div>
     );
