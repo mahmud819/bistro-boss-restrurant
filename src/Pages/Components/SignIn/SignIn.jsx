b41c46d7745282556503ab3ac859f69ede0a45fb
mahmud819 <mahmudurr819@gmail.com>
2026-07-13 23:44:35 +0600

    captcha function partialy added

--- src/Pages/Components/SignIn/SignIn.jsx
+++ src/Pages/Components/SignIn/SignIn.jsx
@@ -1,16 +1,34 @@
-import React from 'react';
-import bgImage from '../../../assets/others/authentication.png'
-import authenticImg from '../../../assets/others/authentication2.png'
+import React from "react";
+import bgImage from "../../../assets/others/authentication.png";
+import authenticImg from "../../../assets/others/authentication2.png";
 import { Link } from "react-router-dom";
-import { FaFacebook, FaGithub, FaGoogle } from 'react-icons/fa6';
+import { FaFacebook, FaGithub, FaGoogle } from "react-icons/fa6";
+import {
+  LoadCanvasTemplate,
+  LoadCanvasTemplateNoReload,
+  loadCaptchaEnginge,
+} from "react-simple-captcha";
 
 const SignIn = () => {
-    return (
-        <div className="hero  min-h-screen" style={{ backgroundImage: `url(${bgImage})` }}>
+  loadCaptchaEnginge(6);
+
+  const handleSignIn = (e) => {
+    e.preventDefault();
+    const form = e.target;
+
+    const email = form.email.value;
+    const captcha = form.captcha.value;
+    console.log(email,captcha);
+  };
+  return (
+    <div
+      className="hero  min-h-screen"
+      style={{ backgroundImage: `url(${bgImage})` }}
+    >
       <div className="hero-content flex-col lg:flex-row">
         <div className="text-center lg:text-left">
           <h1 className="text-5xl font-bold text-center">Sign In Now!</h1>
-          
+
           <div>
             <img src={authenticImg} alt="" />
           </div>
@@ -18,26 +36,54 @@ const SignIn = () => {
         {/* Sign Up form */}
         <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
           <div className="card-body">
-            <fieldset className="fieldset">
-              <label className="label">Email</label>
-              <input type="email" className="input outline-none" placeholder="Email" />
-              <label className="label">Password</label>
-              <input type="password" className="input outline-none" placeholder="Password" />
-              <input type="text" className="input outline-none" placeholder="" />
-              <button className="btn mt-4 bg-[#D99904] text-white font-bold">Sign In</button>
-              <p className="text-center text-[#D98904]">Don't have any Account? <Link to='/signUp' className="font-bold">Register Here</Link></p>
-              <p className='text-center font-bold '>Or</p>
-            </fieldset>
-            <div className='flex text-lg justify-center items-center'>
-                <FaFacebook className=''></FaFacebook>
-                <FaGoogle className='ml-6'></FaGoogle>
-                <FaGithub className='ml-6'></FaGithub>
+            <form action="" onSubmit={handleSignIn}>
+              <fieldset className="fieldset">
+                <label className="label">Email</label>
+                <input
+                  type="email"
+                  className="input outline-none"
+                  placeholder="Email"
+                  name="email"
+                />
+                <label className="label">Password</label>
+                <input
+                  type="password"
+                  className="input outline-none"
+                  placeholder="Password"
+                />
+                <input
+                  type="text"
+                  className="input outline-none"
+                  placeholder="Write here below Captcha"
+                  name="captcha"
+                />
+                <LoadCanvasTemplate />
+                {/* <LoadCanvasTemplateNoReload /> */}
+                <button
+                  
+                  className="btn mt-4 bg-[#D99904] text-white font-bold"
+                >
+                  Sign In
+                </button>
+                <p className="text-center text-[#D98904]">
+                  Don't have any Account?{" "}
+                  <Link to="/signUp" className="font-bold">
+                    Register Here
+                  </Link>
+                </p>
+                <p className="text-center font-bold ">Or</p>
+              </fieldset>
+            </form>
+            <div className="flex text-lg justify-center items-center">
+              <FaFacebook className=""></FaFacebook>
+              <FaGoogle className="ml-6"></FaGoogle>
+              <FaGithub className="ml-6"></FaGithub>
             </div>
           </div>
         </div>
       </div>
     </div>
-    );
+  );
 };
 
 export default SignIn;
