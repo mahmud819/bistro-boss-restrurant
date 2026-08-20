ffdce350de67c2880d986b45ddc240cd79322fb7
mahmud819 <mahmudurr819@gmail.com>
2026-08-20 07:01:50 +0600

    my cart component some design add

--- src/Dashboard/UserPages/MyCart/MyCart.jsx
+++ src/Dashboard/UserPages/MyCart/MyCart.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import SharedDashBoardTitle from '../../../Shared/SharedComponent/SharedDashBoardTitle/SharedDashBoardTitle';
+import useCart from '../../../CustomHook/UseCart/useCart';
 
 const MyCart = () => {
+
+    const[cart]=useCart();
+    const totalPrice = cart.reduce((total,item)=>total+item?.price,0)
     return (
         <div>
             <SharedDashBoardTitle title={'WANNA ADD MORE?'} subTitle={'My Cart'}></SharedDashBoardTitle>
+            <div className='flex justify-evenly'>
+                <h1 className='font-bold text-2xl'>Total Order : {cart?.length} </h1>
+                <h1 className='font-bold text-2xl'>Total Price : {totalPrice} </h1>
+                <button className='btn bg-[#D99904] font-bold text-xl'>Pay</button>
+            </div>
         </div>
     );
 };
--- src/Shared/SharedComponent/SharedDashBoardTitle/SharedDashBoardTitle.jsx
+++ src/Shared/SharedComponent/SharedDashBoardTitle/SharedDashBoardTitle.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const SharedDashBoardTitle = ({subTitle,title}) => {
     return (
-        <div className='flex flex-col mx-auto justify-center items-center text-center w-full'>
+        <div className='pt-6 mx-auto text-center w-1/3'>
             {subTitle&&<p className='text-[#D99904] text-md mb-2'>---{subTitle}---</p>}
             {title&&<h1 className='border-y-2 border-gray-400 py-1 text-2xl font-bold uppercase mb-12'>{title}</h1>}
 
